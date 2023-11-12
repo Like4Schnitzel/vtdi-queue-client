0fea4b1a3d8975602da4f170de1832d91dc8c91c
Schnitzel <rw21062006@gmail.com>
2023-11-12 12:29:30 +0100

    move to main function

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from json import loads
 from requests import get
 from sseclient import SSEClient
 
-env_json = open('env.json', encoding='utf-8')
-env_vars = load(env_json)
+def main():
+    env_json = open('env.json', encoding='utf-8')
+    env_vars = load(env_json)
 
-queue = loads(get(f'{env_vars["URL"]}/queue', timeout=5).text)['videos']
-pprint(queue)
-
-messages = SSEClient(f'{env_vars["URL"]}/api/sse')
-
-for msg in messages:
-    if msg.event == 'queueItemAdded':
-        queue.append(loads(msg.data))
-    if msg.event == 'queueItemRemoved':
-        queue.pop(loads(msg.data))
+    queue = loads(get(f'{env_vars["URL"]}/queue', timeout=5).text)['videos']
     pprint(queue)
+
+    messages = SSEClient(f'{env_vars["URL"]}/api/sse')
+
+    for msg in messages:
+        if msg.event == 'queueItemAdded':
+            queue.append(loads(msg.data))
+        if msg.event == 'queueItemRemoved':
+            queue.pop(loads(msg.data))
+        pprint(queue)
+
+if __name__ == '__main__':
+    main()
