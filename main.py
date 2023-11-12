8a85eaba13971d8eaf210011f5bcaf84818bdcb9
Schnitzel <rw21062006@gmail.com>
2023-11-12 11:49:39 +0100

    add and remove queue items

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from json import load
+from json import loads
 from sseclient import SSEClient
 
 env_json = open('env.json', encoding='utf-8')
 env_vars = load(env_json)
 
+queue = []
+
 messages = SSEClient(f'{env_vars["URL"]}/api/sse')
 
 for msg in messages:
-    print(msg)
+    if msg.event == 'queueItemAdded':
+        queue.append(loads(msg.data))
+    if msg.event == 'queueItemRemoved':
+        queue.pop(loads(msg.data))
+    print(queue)
